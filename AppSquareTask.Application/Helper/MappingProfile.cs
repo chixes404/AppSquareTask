06dd727c1e99965defaaca596edee124470981bd
basel.31057579@ics.tanta.edu.eg <basel.31057579@ics.tanta.edu.eg>
2024-10-17 11:11:22 +0300

    feat : map from registerModel into applicationUser

---
+++ AppSquareTask.Application/Helper/MappingProfile.cs
@@ -0,0 +1,29 @@
+﻿using AppSquareTask.Application.MediatrHandelr.Auth.CustomerRegister;
+using AppSquareTask.Application.MediatrHandelr.Auth.Register;
+using AppSquareTask.Core.Models;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppSquareTask.Application.Helper
+{
+	public class MappingProfile : Profile
+	{
+		public MappingProfile()
+		{
+			CreateMap<OwnerRegisterCommand, ApplicationUser>();
+			CreateMap<CustomerRegisterCommand, ApplicationUser>();
+
+
+
+
+		}
+
+
+
+	}
+}
